ce79015a8c0024040094fee4f8164efa9ef57ffb
J. Oleson <68702502+Oleson-Cybersec@users.noreply.github.com>
2023-05-09 16:24:17 -0400

    Create Delete-Bad-Directories.ps1

---
+++ Delete-Bad-Directories.ps1
@@ -0,0 +1,5 @@
+Remove-Item -LiteralPath "C:\Program Files\DriverUpdate" -Force -Recurse
+Remove-Item -LiteralPath "C:\Program Files\SlimCleaner Plus" -Force -Recurse
+Remove-Item -LiteralPath "C:\Program Files\SlimWare Utilities" -Force -Recurse
+Remove-Item -LiteralPath "C:\Program Files\SlimServices" -Force -Recurse
+Remove-Item -LiteralPath "C:\Program Files (x86)\ConsumerSoft" -Force -Recurse
